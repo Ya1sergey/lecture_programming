d1356b38e8f76f8d648b61a8299f175cdd085898
Ya1sergey <Ya1sergey12@gmail.com>
2023-07-09 15:19:24 +0900

    random

--- lesson_1/Example_003_sum/Program.cs
+++ lesson_1/Example_003_sum/Program.cs
@@ -1,3 +1,5 @@
-﻿int NumberA = 1;
-int NumberB = 2;
+﻿int NumberA = new Random().Next(1, 100); // 1 2 3 4 .... 99
+int NumberB = new Random().Next(1, 100);
+Console.WriteLine(NumberA);
+Console.WriteLine(NumberB);
 Console.WriteLine(NumberA + NumberB);

9c65157c2020c038cccfa7dbf2d4f0d6e54a47af
Ya1sergey <Ya1sergey12@gmail.com>
2023-07-09 20:10:21 +0900

    magic

---
+++ lesson_1/Example_007_magic/Program.cs
@@ -0,0 +1,44 @@
+﻿Console.Clear();
+int xa = 20, ya = 1,
+    xb = 1, yb = 20,
+    xc = 40, yc = 20;
+
+Console.SetCursorPosition(xa, ya);
+Console.Write("+");
+
+Console.SetCursorPosition(xb, yb);
+Console.Write("+");
+
+Console.SetCursorPosition(xc, yc);
+Console.Write("+");
+
+int x = xa, y = ya;
+
+int count = 0;
+
+while (count < 100000)
+{
+    int what = new Random().Next(0, 3);
+    if(what == 0)
+    {
+        x = (x + xa) / 2;
+        y = (y + ya) / 2;
+    }
+
+        if(what == 1)
+    {
+        x = (x + xb) / 2;
+        y = (y + yb) / 2;
+    }
+
+    if(what == 2)
+    {
+        x = (x + xc) / 2;
+        y = (y + yc) / 2;
+    }
+
+    Console.SetCursorPosition(x, y);
+    Console.WriteLine("+");
+    count++;
+
+}

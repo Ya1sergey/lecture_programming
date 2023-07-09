0d5516a9f00eb2db6e4a714130d11545013634f1
Ya1sergey <Ya1sergey12@gmail.com>
2023-07-09 19:21:00 +0900

    base

---
+++ lesson_1/Example_006_base/Program.cs
@@ -0,0 +1,15 @@
+﻿int a = 1;
+int b = 8;
+int c = 12;
+int d = 12;
+int e = 6;
+
+int max = a;
+
+if(max < b) max = b;
+if(max < c) max = c;
+if(max < d) max = d;
+if(max < e) max = e;
+
+Console.Write("max = ");
+Console.WriteLine(max);
